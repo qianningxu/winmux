cb6fd632c6744bd1ed1d051030ff88c4c981c809
qianningxu <qx20@st-andrews.ac.uk>
2026-09-07 14:49:44 +0100

    Use gray 800 for window stack shell

--- Sources/AppBundle/ui/WindowTabGroupFrameView.swift
+++ Sources/AppBundle/ui/WindowTabGroupFrameView.swift
@@ -9,7 +9,7 @@ struct WindowTabGroupFrameView: View {
 
     var body: some View {
         RoundedRectangle(cornerRadius: windowTabStripCornerRadius + windowTabGroupShellHorizontalInset(), style: .continuous)
-            .fill(palette.color(.gray, .color10))
+            .fill(palette.color(.gray, .color8))
             .overlay {
                 RoundedRectangle(cornerRadius: windowTabStripCornerRadius + windowTabGroupShellHorizontalInset(), style: .continuous)
                     .strokeBorder(palette.color(.gray, .color5), lineWidth: WinMuxBarStyle.strokeWidth)
